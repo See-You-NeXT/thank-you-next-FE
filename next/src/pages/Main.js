0afaf44f076b6ae16a48930ff0df5787614b0b0a
wlthdud <thdudzi@naver.com>
2023-12-21 23:58:30 +0900

    feat: 메인페이지 디자인 완성

--- next/src/pages/Main.js
+++ next/src/pages/Main.js
@@ -22,20 +22,32 @@ function Main(){
             <div className={styles.contentArea}>
                 <div className={styles.videoArea}>
                     <div className={styles.videoTitle}>THIS IS NeXT 🔥</div>
-                    {/* <div className={styles.video}></div> */}
                     <video muted autoPlay loop>
                         <source src="/23next.MP4" type="video/mp4" />
                     </video>
                 </div>
                 <div className={styles.newBoardArea}>
                     <div className={styles.boardTitle}>게시판 최신글 📢</div>
-                        <div className={styles.boardTabArea}>
-                            <div className={styles.tabItem}>공지</div>
-                            <div className={styles.tabItem}>질문</div>
-                            <div className={styles.tabItem}>자유</div>
-                        </div>
+                    <div className={styles.boardTabArea}>
+                        <div className={styles.tabItem}>공지</div>
+                        <div className={styles.tabItem}>질문</div>
+                        <div className={styles.tabItem}>자유</div>
+                    </div>
                     <div className={styles.board}>
-                        게시판
+                        <ul className={styles.boardUl}>
+                            <li className={styles.boardLi}>
+                                17일 지소영 슨배님 졸업식 참여자 수요...
+                                <span>12.XX</span>
+                            </li>
+                            <li className={styles.boardLi}>
+                                개강파티 수요조사를 시작합니다!
+                                <span>12.XX</span>
+                            </li>
+                            <li className={styles.boardLi}>
+                                이번주 세미나는 없습니다.
+                                <span>12.XX</span>
+                            </li>
+                        </ul>
                     </div>
                     <div className={styles.plusBtn}>더보기</div>
                 </div>
