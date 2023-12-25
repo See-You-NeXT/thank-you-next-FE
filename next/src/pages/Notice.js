140007f9a5d7566c687f8747947ff48106d922c7
wlthdud <thdudzi@naver.com>
2023-12-25 22:50:45 +0900

    feat: 공지게시판 글목록 임시데이터 테스트

--- next/src/pages/Notice.js
+++ next/src/pages/Notice.js
@@ -1,6 +1,17 @@
 import styles from './Notice.module.css';
+import { useState } from "react";
+import Pagination from "react-js-pagination";
 
-function Notice(){
+import data from '../data'; 
+
+function Notice(props){
+
+    let [list] = useState(data);
+    const [page, setPage] = useState(1);
+    const handlePageChange = (page) => {
+        setPage(page);
+      };
+
     return(
         <div className={styles.notice}>
             <div className={styles.textArea}>
@@ -27,20 +38,27 @@ function Notice(){
                     <div className={styles.listTitleItems}>제목</div>
                     <div className={styles.listTitleItems}>날짜</div>
                 </div>
-                <div className={styles.listContent}>
-                    <div className={styles.listContentItems}>양승민</div>
-                    <div className={styles.listContentItems}>안녕하세요줄바꿈테스트입니다안녕하세요줄바꿈테스트입니다안녕하세요줄바꿈테스트입니다</div>
-                    <div className={styles.listContentItems}>XX.XX</div>
-                </div>
-                <div className={styles.listContent}>
-                    <div className={styles.listContentItems}>양승민</div>
-                    <div className={styles.listContentItems}>안녕하세요줄바꿈테스트입니다안녕하세요줄바꿈테스트입니다안녕하세요줄바꿈테스트입니다</div>
-                    <div className={styles.listContentItems}>XX.XX</div>
-                </div>
+                 {/* 데이터 역순으로 보내주세용 */}
+                {
+                    list.reverse().map((a,i)=>{
+                        return(
+                            <ListContent list={list[i]}/>
+                        )
+                    })
+                }
             </div>
-            
         </div>
     );
+}
+
+function ListContent(props){
+    return(
+        <div className={styles.listContent}>
+            <div className={styles.listContentItems}>{props.list.name}</div>
+            <div className={styles.listContentItems}>{props.list.title}</div>
+            <div className={styles.listContentItems}>{props.list.date}</div>
+        </div>
+    )
 }
 
 export default Notice;
