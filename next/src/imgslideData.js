ec7dc73a57c7fe7dd91e071fdaa755eff86deeca
wlthdud <thdudzi@naver.com>
2024-01-03 04:51:50 +0900

    feat: 이미지슬라이드 기능 + 슬라이드 아이콘

--- next/src/imgslideData.js
+++ next/src/imgslideData.js
@@ -1,25 +1,26 @@
 let imgslideData = [
     {   
-        id : 1,
+        id : 0,
         img : '/ysm.png',
-        name : "양승민 선배님",
+        name : "재학생 양승민",
         title : "양승민의 코딩콘서트",
         info : "C, C++, Java 없는게 없다!",
         subInfo : "백준 플레티넘 선배님과 함께 하는 코딩 공부"
     },
 
     {
-        id : 2,
-        name : "김창식 선배님",
+        id : 1,
+        img : '/kcs.png',
+        name : "회장 김창식",
         title : "김창식의 이렐리아 강의",
         info : "1997은 잊어라.",
         subInfo : "모두에게 감동을 선사하는 김창식의 이렐리아 활용법!"
     },
 
     {
-        id : 3,
+        id : 2,
         img : '/csh.png',
-        name : "최선학 선배님",
+        name : "졸업생 최선학",
         title : "최선학의 전역 늦게 하는 법",
         info : "모두가 전역하고 싶어할 때 남들과는 다른 길을 가라"
     },
--- next/src/pages/Main.js
+++ next/src/pages/Main.js
@@ -1,5 +1,5 @@
 import styles from './Main.module.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GoDot } from "react-icons/go";
 import { GoDotFill } from "react-icons/go";
 
@@ -7,15 +7,43 @@ import imgslideData from '../imgslideData';
 
 function Main(){
     let [imgslide] = useState(imgslideData);
+    let [imgIdx, setImgIdx] = useState(0);
+    let [currentSlideIcon, setCurrentSlideIcon] = useState(0);
+   
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            nextSlide();
+        },5000);
+        return () => clearInterval(interval); 
+    }, [imgIdx]);
+
+    const nextSlide = () => {
+        setImgIdx((preIdx) => (preIdx+1) % imgslide.length);
+        setCurrentSlideIcon((preIdx) => (preIdx+1) % imgslide.length);
+    };
+
+    const currentIcon = (i) => {
+        setImgIdx(i);
+        setCurrentSlideIcon(i)
+    }
 
     return(
         <div className={styles.main}>
-            <Imgslide imgslide={imgslide[0]} />
+            <Imgslide imgslide={imgslide[imgIdx]} />
             
-            <div className={styles.pageIcons}>
-                <GoDotFill size={20}/>
-                <GoDot size={20}/>
-                <GoDot size={20}/>
+            <div className={styles.pageIconsArea}>
+                {
+                    imgslide.map((a,i)=>{
+                        return(
+                            <div className={styles.pageIcons}
+                                onClick={()=>currentIcon(i)}>
+                            {
+                                currentSlideIcon == i ? <GoDotFill size={20}/> : <GoDot size={20}/>
+                            }
+                            </div>
+                        )
+                    })
+                }
             </div>
 
             <div className={styles.contentArea}>
@@ -58,7 +86,10 @@ function Main(){
 function Imgslide(props){
     return(
         <div className={styles.imgslideArea}>
-            <img src={props.imgslide.img} />
+            <div className={styles.imgArea}>
+                <img src={props.imgslide.img} />
+            </div>
+            
             <div className={styles.imgslideContent}>
                 <div className={styles.slideName}>{props.imgslide.name}</div>
                 <div className={styles.slideTitle}>{props.imgslide.title}</div>
