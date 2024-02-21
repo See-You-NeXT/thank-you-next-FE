6f0be12b48351773e300d9b7bef11c9aa2b42b42
wlthdud <thdudzi@naver.com>
2024-02-21 23:44:33 +0900

    feat: 스터디 소개페이지 FE 제작, FE/BE버튼 구현

--- next/src/pages/Introduce.js
+++ next/src/pages/Introduce.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './Introduce.module.css';
 import NeXTNav from '../components/NeXTNav';
 
 function Introduce() {
+  const [fe, setFe] = useState(true);
+  // const [be, setBe] = useState(false);
+
+  const handleFeBtn = () => {
+    setFe(true);
+    // setBe(false);
+  }
+
+  const handleBeBtn = () => {
+    // setBe(true);
+    setFe(false);
+  }
+
   return (
     <div className={styles.introduce}>
       <NeXTNav currentPage={"스터디 소개"}/>
       <div className={styles.originArea}>
         <img src='/origin.png'/>
+      </div>
+      <div className={styles.developerArea}>
+        <div className={styles.developerTitle}>
+          개발진 소개
+        </div>
+        <div className={styles.selectFeBe}>
+          <div className={styles.feBtn} onClick={handleFeBtn}
+            style={{backgroundColor:fe?"#ffefe9":"white"}}>
+            Front-End
+          </div>
+          <div className={styles.beBtn} onClick={handleBeBtn}
+            style={{backgroundColor:!fe?"#ffefe9":"white"}}>
+            Back-End
+          </div>
+        </div>
+        <div className={styles.developer}>
+          {
+            fe ? <img src='/developerImg/fe.png' /> : <img src='/origin.png'/>
+          }
+          
+        </div>
       </div>
     </div>
   );
